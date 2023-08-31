af22bff5ea29ceae209d4ce9ed94f7a6d307e7b1
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-08-31 11:41:30 +0100

    Price Change Function

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const AnnualButton = document.getElementById('Annual-Button');
-    const prices = {
-      'Basic-Price': '&dollar;19.99',
-      'Professional-Price': '&dollar;24.99',
-      'Master-Price': '&dollar;39.99'
-    };
-    
-    AnnualButton.addEventListener('click', function () {
-      const icon = AnnualButton.querySelector('i');
-      const priceElements = document.querySelectorAll('.p1-1');
-    
-      icon.classList.toggle('fa-toggle-on');
-      icon.classList.toggle('fa-toggle-off');
-    
-      priceElements.forEach(priceElement => {
-        const originalPrice = prices[priceElement.id];
-        const currentPrice = priceElement.innerHTML;
-    
-        if (currentPrice === originalPrice) {
-          priceElement.innerHTML = 'Annual price: ' + originalPrice;
-        } else {
-          priceElement.innerHTML = originalPrice;
-        }
-      });
-    });
-  });
-  
+// Get references to the checkbox and price elements
+const checkbox = document.querySelector('input[type="checkbox"]');
+const basicPrice = document.getElementById('Basic-Price');
+const professionalPrice = document.getElementById('Professional-Price');
+const masterPrice = document.getElementById('Master-Price');
+
+// Define the original prices
+const originalPrices = {
+  basic: '$199.99',
+  professional: '$249.99',
+  master: '$399.99'
+};
+
+// Define the discounted prices
+const discountedPrices = {
+  basic: '$19.99',
+  professional: '$24.99',
+  master: '$39.99'
+};
+
+// Function to update prices based on checkbox status
+function updatePrices() {
+  if (checkbox.checked) {
+    basicPrice.textContent = discountedPrices.basic;
+    professionalPrice.textContent = discountedPrices.professional;
+    masterPrice.textContent = discountedPrices.master;
+  } else {
+    basicPrice.textContent = originalPrices.basic;
+    professionalPrice.textContent = originalPrices.professional;
+    masterPrice.textContent = originalPrices.master;
+  }
+}
+
+// Attach event listener to the checkbox
+checkbox.addEventListener('change', updatePrices);
+
+// Initial call to set prices based on checkbox status
+updatePrices();
